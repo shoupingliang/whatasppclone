1a6eb161be6ef685a3e0bb00a1513d6edc79a8dd
Shouping Liang <shoupingliang@gmail.com>
2022-04-15 13:22:37 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build a whatapp clone!</h1>
+      <div className="app__body">
+        < Sidebar />
+        {/* {Chat} */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBAbSGA_C1p-S0H7Gh0Abb7_dzb1od7Wu0",
+    authDomain: "whatapp-bd1c3.firebaseapp.com",
+    projectId: "whatapp-bd1c3",
+    storageBucket: "whatapp-bd1c3.appspot.com",
+    messagingSenderId: "927486936099",
+    appId: "1:927486936099:web:b23ae8eef03d240e351d96",
+    measurementId: "G-EXMG1JFGXY"
+  };
